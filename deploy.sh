29fe2375b6fcde061c2d77ec752ffb390a167cab
576625322@qq.com <hzyandkyn1314>
2017-01-05 17:08:38 +0800

    add bash

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Set Git config
+git config --global user.name "ascoders"
+git config --global user.email 576625322@qq.com
+git remote add deploy git@github.com:ascoders/wokugame.git
+
+# Checkout to built branch
+git checkout -b built deploy/built
+git add -A
+git commit -m "build"
+
+# Push to built branch
+git push deploy built
