b835226526a057b1bb478daff0f79be885f792dc
576625322@qq.com <hzyandkyn1314>
2017-01-05 19:46:03 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -5,27 +5,10 @@ git config --global user.name "ascoders"
 git config --global user.email 576625322@qq.com
 git remote add deploy git@github.com:ascoders/wokugame.git
 
-# Checkout to built branch
-#git remote -v
-#git branch -a
-#
-#echo "set branches"
-#git remote set-branches --add deploy built
-#git fetch
-#
-#echo "set finish"
-#
-#git remote -v
-#git branch -a
-#
-#git checkout -b built -- deploy/built
-
-git branch built deploy/built
-
-git checkout built
-
+git clone git@github.com:ascoders/wokugame.git branchs
+cd branchs
+git checkout -b built -- origin/built
+mv ../built-production ./
 git add -A
-git commit -m "build"
-
-# Push to built branch
-git push deploy built
+git commit -m "travis build"
+git push
