2b701c13315374d02a8b059dfa0c55f7f2ebccbe
576625322@qq.com <hzyandkyn1314>
2017-01-05 19:07:28 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ git remote add deploy git@github.com:ascoders/wokugame.git
 # Checkout to built branch
 git remote -v
 git checkout -b built
-git reset --hard origin/built
+git reset --hard deploy/built
 git add -A
 git commit -m "build"
 
