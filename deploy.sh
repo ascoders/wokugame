2b4c905c88024f7a7c98c635b6147395cd5ceb80
576625322@qq.com <hzyandkyn1314>
2017-01-05 19:35:04 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -6,19 +6,24 @@ git config --global user.email 576625322@qq.com
 git remote add deploy git@github.com:ascoders/wokugame.git
 
 # Checkout to built branch
-git remote -v
-git branch -a
+#git remote -v
+#git branch -a
+#
+#echo "set branches"
+#git remote set-branches --add deploy built
+#git fetch
+#
+#echo "set finish"
+#
+#git remote -v
+#git branch -a
+#
+#git checkout -b built -- deploy/built
 
-echo "set branches"
-git remote set-branches --add deploy built
-git fetch
+git branch built deploy/built
 
-echo "set finish"
+git checkout built
 
-git remote -v
-git branch -a
-
-git checkout -b built -- deploy/built
 git add -A
 git commit -m "build"
 
