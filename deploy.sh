064077bcc751080443364345ddd13f0547074fd3
576625322@qq.com <hzyandkyn1314>
2017-01-05 17:12:15 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ git config --global user.email 576625322@qq.com
 git remote add deploy git@github.com:ascoders/wokugame.git
 
 # Checkout to built branch
+git fetch
 git checkout -b built deploy/built
 git add -A
 git commit -m "build"
