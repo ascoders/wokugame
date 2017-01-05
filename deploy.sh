a1396b1653d12d49d292a5bf5469aa62628cc6cd
576625322@qq.com <hzyandkyn1314>
2017-01-05 20:51:40 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -5,15 +5,13 @@ git config --global user.name "ascoders"
 git config --global user.email 576625322@qq.com
 git remote add deploy git@github.com:ascoders/wokugame.git
 
-git clone git@github.com:ascoders/wokugame.git branchs
+# Clone built branch
+git clone -b built git@github.com:ascoders/wokugame.git branchs
 cd branchs
 
-echo "123"
-git remote -v
-git branch -a
-
-git checkout -b built origin/built
+rm -rf built-production
 mv ../built-production ./
+
 git add -A
 git commit -m "travis build"
-git push
+git push origin built
