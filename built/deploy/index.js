960ab0a9572384f25606ef5e8b8c9014caf205ca
ascoders <576625322@qq.com>
2017-01-08 14:33:27 +0000

    travis build

--- built/deploy/index.js
+++ built/deploy/index.js
@@ -1,8 +1,10 @@
 "use strict";
 const http = require("http");
 const createHandler = require("github-webhook-handler");
+const child_process_1 = require("child_process");
 const config = require("../config");
 const handler = createHandler({ path: '/webhook', secret: '123456' });
+child_process_1.execSync(`npm run pm2-start`);
 http.createServer((req, res) => {
     handler(req, res, (err) => {
         res.statusCode = 404;
@@ -10,8 +12,13 @@ http.createServer((req, res) => {
     });
 }).listen(config.deployPort);
 handler.on('push', (event) => {
-    console.log('Received a push event for %s to %s', event.payload.repository.name, event.payload.ref);
-    console.log(123123, event);
+    if (event.payload.ref === 'refs/heads/built') {
+        child_process_1.execSync(`rm -rf /app`);
+        child_process_1.execSync(`git clone -b built --depth 1 https://github.com/ascoders/wokugame.git /app`);
+        child_process_1.execSync(`cd /app`);
+        child_process_1.execSync(`yarn`);
+        child_process_1.execSync(`npm run pm2-restart`);
+    }
 });
 process.on('SIGINT', () => {
 });
