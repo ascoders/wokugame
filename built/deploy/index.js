699ca2e454433c84f1b1cad69cae6714fa7fa092
ascoders <576625322@qq.com>
2017-01-10 05:15:44 +0000

    travis build

--- built/deploy/index.js
+++ built/deploy/index.js
@@ -4,19 +4,32 @@ const createHandler = require("github-webhook-handler");
 const child_process_1 = require("child_process");
 const config = require("../config");
 const handler = createHandler({ path: '/webhook', secret: '123456' });
-child_process_1.execSync(`npm run pm2-start`);
+child_process_1.execSync(`npm run deploy`);
 http.createServer((req, res) => {
-    handler(req, res, (err) => {
-        res.statusCode = 404;
-        res.end('no such location');
-    });
+    if (req.url === '/webhook') {
+        handler(req, res, (err) => {
+            res.statusCode = 404;
+            res.end('no such location');
+        });
+    }
+    const options = {
+        host: 'localhost',
+        port: config.localPort,
+        path: req.url,
+        method: req.method
+    };
+    http.request(options, response => {
+        response.pipe(res);
+        console.log(req.url);
+    }).end();
 }).listen(config.deployPort);
 handler.on('push', (event) => {
     if (event.payload.ref === 'refs/heads/built') {
+        child_process_1.execSync(`cp /app/node_modules /app-cache`);
         child_process_1.execSync(`rm -rf /app`);
         child_process_1.execSync(`git clone -b built --depth 1 https://github.com/ascoders/wokugame.git /app`);
+        child_process_1.execSync(`mv /app-cache /app/node_modules`);
         child_process_1.execSync(`cd /app`);
-        child_process_1.execSync(`yarn`);
         child_process_1.execSync(`npm run pm2-restart`);
     }
 });
