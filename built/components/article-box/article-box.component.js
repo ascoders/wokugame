b43f13fe6bb638083fb0dac997a23e4d23006186
ascoders <576625322@qq.com>
2017-01-10 14:10:31 +0000

    travis build

--- built/components/article-box/article-box.component.js
+++ built/components/article-box/article-box.component.js
@@ -6,9 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = (props = new typings.Props()) => {
     return (React.createElement("div", { className: styles.container },
         React.createElement("div", { className: styles.leftContainer },
-            React.createElement("div", { className: styles.categoryContainer }, "\u8BDD\u98981"),
-            React.createElement("div", { className: styles.titleContainer }, "\u6807\u98982"),
-            React.createElement("div", { className: styles.otherContainer }, "\u65E5\u671F3")),
+            React.createElement("div", { className: styles.categoryContainer }, "\u8BDD\u9898"),
+            React.createElement("div", { className: styles.titleContainer }, "\u6807\u9898"),
+            React.createElement("div", { className: styles.otherContainer }, "\u65E5\u671F")),
         React.createElement("div", { className: styles.rightContainer })));
 };
 //# sourceMappingURL=article-box.component.js.map
--- built/src/client/html.js
+++ built/src/client/html.js
@@ -5,7 +5,7 @@ let scripts;
 if (isProduction) {
     scripts = `
         <script src='/${config.publicPath}/dll/library.6d8f1.dll.js'></script>
-        <script src='/${config.publicPath}/bundle.be001.js'></script>
+        <script src='/${config.publicPath}/bundle.9cc57.js'></script>
     `;
 }
 else {
