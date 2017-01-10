c72cc76a0d808b923f2f1bea19e76f82af1bbcee
576625322@qq.com <hzyandkyn1314>
2017-01-10 21:22:02 +0800

    update

--- components/article-box/article-box.component.tsx
+++ components/article-box/article-box.component.tsx
@@ -9,11 +9,11 @@ export default (props: typings.PropsDefine = new typings.Props()) => {
             <div className={styles.leftContainer}>
                 {/* 类别容器 */}
                 <div className={styles.categoryContainer}>
-                    777
+                    话题
                 </div>
                 {/* 标题容器 */}
                 <div className={styles.titleContainer}>
-                    标题容器
+                    标题
                 </div>
                 {/* 日期等杂项容器 */}
                 <div className={styles.otherContainer}>
