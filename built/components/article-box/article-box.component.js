07c14cc3b2a715f9cff584928ec8cc16cea33e30
ascoders <576625322@qq.com>
2017-01-11 07:33:10 +0000

    travis build

--- built/components/article-box/article-box.component.js
+++ built/components/article-box/article-box.component.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = (props = new typings.Props()) => {
     return (React.createElement("div", { className: styles.container },
         React.createElement("div", { className: styles.leftContainer },
-            React.createElement("div", { className: styles.categoryContainer }, "\u8BDD\u9898123456"),
+            React.createElement("div", { className: styles.categoryContainer }, "\u8BDD\u98981234567"),
             React.createElement("div", { className: styles.titleContainer }, "\u6807\u9898"),
             React.createElement("div", { className: styles.otherContainer }, "\u65E5\u671F")),
         React.createElement("div", { className: styles.rightContainer })));
