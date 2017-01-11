b3235c3af9421c2a2ae17ea59f169ea558f70423
ascoders <576625322@qq.com>
2017-01-11 06:44:49 +0000

    travis build

--- built/src/client/html.js
+++ built/src/client/html.js
@@ -5,7 +5,7 @@ let scripts;
 if (isProduction) {
     scripts = `
         <script src='/${config.publicPath}/dll/library.6d8f1.dll.js'></script>
-        <script src='/${config.publicPath}/bundle.ddf83.js'></script>
+        <script src='/${config.publicPath}/bundle.91564.js'></script>
     `;
 }
 else {
