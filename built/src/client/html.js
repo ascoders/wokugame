1cdf58b1fb0dd16382587afe87a297a43c8879cf
ascoders <576625322@qq.com>
2017-01-11 03:55:45 +0000

    travis build

--- built/src/client/html.js
+++ built/src/client/html.js
@@ -5,7 +5,7 @@ let scripts;
 if (isProduction) {
     scripts = `
         <script src='/${config.publicPath}/dll/library.6d8f1.dll.js'></script>
-        <script src='/${config.publicPath}/bundle.21063.js'></script>
+        <script src='/${config.publicPath}/bundle.41d27.js'></script>
     `;
 }
 else {
