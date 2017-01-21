cb301228f3ba3d85c3fab633b0e1b1579780431f
ascoders <576625322@qq.com>
2017-01-21 18:27:07 +0000

    travis build

--- built/src/client/html.js
+++ built/src/client/html.js
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV === 'production') {
         <script src='/${config.publicPath}/bundle.d6cfa.js'></script>
     `;
     links = `
-        <link rel="stylesheet" type="text/css" href="/${config.publicPath}/styles/main.jcrx9.css">
+        <link rel="stylesheet" type="text/css" href="/${config.publicPath}/styles/main.o95l3.css">
     `;
 }
 else {
